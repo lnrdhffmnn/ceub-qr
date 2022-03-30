5893d11ab7b37795979b5f8b49bb403a543c0ff8
lnrdhffmnn <90478165+lnrdhffmnn@users.noreply.github.com>
2022-03-30 12:15:20 -0300

    Opção de ler um código QR

--- src/components/settings-modal/SettingsModal.js
+++ src/components/settings-modal/SettingsModal.js
@@ -1,14 +1,37 @@
 import React, { useRef, useState } from 'react';
-import { Button, FormControl, FormLabel, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, NumberInput, NumberInputField, useColorModeValue } from '@chakra-ui/react';
+import { Button, FormControl, FormLabel, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, NumberInput, NumberInputField, useColorModeValue } from '@chakra-ui/react';
 import { isMobile } from 'react-device-detect';
+import { Decoder } from '@nuintun/qrcode';
 
 function SettingsModal({ isOpen, onClose, onCloseSave, curRa, curCod }) {
   const [ra, setRa] = useState('');
   const [codigo, setCodigo] = useState('');
 
   const inputFocus = useRef();
+  const fileInputRef = useRef();
 
   const inputBorderColor = useColorModeValue('purple.600', 'purple.300');
+
+  const qrcode = new Decoder();
+
+  async function handleFileInputChange(file) {
+    let source = URL.createObjectURL(file);
+    let sep = '|';
+
+    try {
+      let result = await qrcode.scan(source);
+      let data = result.data;
+
+      if (!data.includes(sep)) throw new Error('QR Code inválido');
+
+      let [_ra, _cod] = data.split(sep);
+
+      setRa(_ra);
+      setCodigo(_cod);
+    } catch (err) {
+      alert(err.message || 'Não foi possível ler o código');
+    }
+  }
 
   return (
     <Modal
@@ -29,6 +52,7 @@ function SettingsModal({ isOpen, onClose, onCloseSave, curRa, curCod }) {
               min={0}
               variant='outline'
               focusBorderColor={inputBorderColor}
+              value={ra}
               onChange={(t) => { setRa(t) }}
             >
               <NumberInputField placeholder={curRa} ref={inputFocus} />
@@ -41,6 +65,7 @@ function SettingsModal({ isOpen, onClose, onCloseSave, curRa, curCod }) {
               clampValueOnBlur={false}
               variant='outline'
               focusBorderColor={inputBorderColor}
+              value={codigo}
               onChange={(t) => { setCodigo(t) }}
             >
               <NumberInputField placeholder={curCod} />
@@ -48,10 +73,26 @@ function SettingsModal({ isOpen, onClose, onCloseSave, curRa, curCod }) {
           </FormControl>
         </ModalBody>
         <ModalFooter>
+          <Input
+            type='file'
+            display='none'
+            accept='image/*'
+            ref={fileInputRef}
+            onChange={(e) => { handleFileInputChange(e.target.files[0]) }}
+          />
+          <Button
+            colorScheme='purple'
+            w='100%'
+            mb={3}
+            mr={3}
+            onClick={() => { fileInputRef.current.click() }}
+          >
+            Ler QR Code
+          </Button>
           <Button
             colorScheme='whatsapp'
             type='submit'
-            w='100vh'
+            w='100%'
             mb={3}
             onClick={() => {
               onCloseSave(ra, codigo);
